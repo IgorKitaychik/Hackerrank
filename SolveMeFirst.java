49f83b56f13ac6453f1f8f8de44c33478b7584a9
IgorKitaychik <34146393+IgorKitaychik@users.noreply.github.com>
2017-11-30 21:14:58 -0500

    Add files via upload

---
+++ SolveMeFirst.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/*
+URL: https://www.hackerrank.com/challenges/solve-me-first/problem
+Description: Warm up exercise.
+*/
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+         // Hint: Type return a+b; below 
+         return a+b;
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
+
